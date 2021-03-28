cd23825b81fe83d551906328f70c783eff8f85f8
hass0n3 <hassen.alfaris@ocsbstudent.ca>
2021-03-28 18:01:33 +0000

    5

--- script.js
+++ script.js
@@ -3,12 +3,22 @@ let dof = 0
 
 document.getElementById('button').addEventListener('click', click)
 
-document.getElementById('button2').addEventListener('click', click)
+document.getElementById('button2').addEventListener('click', click2)
 
 function click () {
   age = document.getElementById('age').value
+  if ((dof === 'tuesday' || dof === 'thursday') || (age > 12 && age < 21)) {
+    alert('You get student pricing')
+  } else {
+    alert('you must pay full admission')
+  }
+}
+
+function click2 () {
   dof = document.getElementById('dayofweek').value
-  if ((dof === 'tuesday' || dof === 'thursday') || (age > 10 && age < 16)) {
-    alert('Yes you just earned a student discount')
+  if ((dof === 'tuesday' || dof === 'thursday') || (age > 12 && age < 21)) {
+    alert('You get student pricing')
+  } else {
+    alert('you must pay full admission')
   }
 }
